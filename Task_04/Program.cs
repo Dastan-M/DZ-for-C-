3d0a46ff61b537e962de4b224553f3b526a6051d
Mr.D <damedetov@mail.rum>
2023-12-01 23:08:20 +0600

    Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дано натуральное число в диапазоне от 1 до 100 000. Создайте массив, 
+// состоящий из цифр этого числа. Старший разряд числа должен располагаться на 0-м индексе массива, младший – на последнем. 
+// Размер массива должен быть равен количеству цифр.
+
+System.Console.WriteLine("Задайте натуральное число в диапазоне от 1 до 100 000: ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+int digitCount = (int)Math.Floor(Math.Log10(number)) + 1;
+int[] digits = new int[digitCount];
+
+for (int i = digitCount - 1; i >= 0; i--)
+{
+    digits[i] = number % 10; 
+    number /= 10; 
+}
+System.Console.Write("Цифры числа в массиве:");
+foreach (var digit in digits)
+{
+    Console.Write(digit);
+}
